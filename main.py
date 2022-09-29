ecedec94acee08cd393f89c241489a4a49155395
Victor Ponce <vicmanuelr@gmail.com>
2022-09-28 19:10:10 -0600

    imported data from csv

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from tkinter import *
 import pandas as pd
 
+# ---------------------------- DATA/PANDAS ------------------------------- #
+data = pd.read_csv("./data/Japanese Words - Alphabet.csv")
+
+def new_card():
+    pass
+
 # ---------------------------- CONSTANTS ------------------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -15,7 +21,7 @@ card_front = PhotoImage(file="./images/card_front.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, columnspan=2, row=0)
 canvas.create_image(400, 263, image=card_front)
-language_title = canvas.create_text(400, 150, text="Title", justify="center")
+language_title = canvas.create_text(400, 150, text="Language", justify="center")
 word_text = canvas.create_text(400, 263, text="Word", justify="center")
 # spelling_text = canvas.create_text(400, 150, text="Title", justify="center")
 
