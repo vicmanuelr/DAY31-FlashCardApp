7d33ec0a78653cca630fb66f883f544bbd34e1a6
Victor Ponce <vicmanuelr@gmail.com>
2022-09-28 23:53:26 -0600

    created back/front card data that will change on timer
    - flip timer added to call flip card
    - converted flip timer global to stop any running timers
    - changed image/text colors for each flip

--- main.py
+++ main.py
@@ -3,34 +3,53 @@ import pandas as pd
 import random as rn
 
 # ---------------------------- CONSTANTS ------------------------------- #
+
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = ("Arial", 40, "italic")
 WORD_FONT = ("noto-cjk", 80, "bold")
+IDLE = 3000
 
 # ---------------------------- DATA/PANDAS ------------------------------- #
+
 data = pd.read_csv("./data/Japanese Words - Alphabet.csv")
-data_pairs = data.to_dict(orient="records")
-title_text = data.columns[0]
+to_learn = data.to_dict(orient="records")
+language_to_learn = data.columns[0]
+translation = data.columns[1]
+current_card = {}
 
 
 def new_card():
-    random_index = rn.randint(0, len(data_pairs)-1)
-    canvas.itemconfigure(word_text, text=data_pairs[random_index][title_text])
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = rn.choice(to_learn)
+    canvas.itemconfigure(card_background, image=front_img)
+    canvas.itemconfigure(card_title, text=language_to_learn, fill="black")
+    canvas.itemconfigure(card_word, text=current_card[language_to_learn], fill="black")
+    flip_timer = window.after(IDLE, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfigure(card_title, text=translation, fill="white")
+    canvas.itemconfigure(card_word, text=current_card[translation], fill="white")
+    canvas.itemconfigure(card_background, image=back_img)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 # Window related
 window = Tk()
 window.title("FlashCardApp")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR, )
+flip_timer = window.after(IDLE, func=flip_card)
 
 # Card canvas
-card_front = PhotoImage(file="./images/card_front.png")
+back_img = PhotoImage(file="./images/card_back.png")
+front_img = PhotoImage(file="./images/card_front.png")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, columnspan=2, row=0)
-canvas.create_image(400, 263, image=card_front)
-language_title = canvas.create_text(400, 150, text=title_text, justify="center", font=LANGUAGE_FONT)
-word_text = canvas.create_text(400, 263, text="Word", justify="center", font=WORD_FONT)
+card_background = canvas.create_image(400, 263, image=front_img)
+card_title = canvas.create_text(400, 150, text="Title", justify="center", font=LANGUAGE_FONT)
+card_word = canvas.create_text(400, 263, text="Word", justify="center", font=WORD_FONT)
 # spelling_text = canvas.create_text(400, 150, text="Title", justify="center")
 
 # Buttons
@@ -40,5 +59,8 @@ right_button.grid(column=1, row=1)
 wrong_img = PhotoImage(file="./images/wrong.png")
 wrong_button = Button(image=wrong_img, highlightthickness=0, command=new_card)
 wrong_button.grid(column=0, row=1)
+
+# Start with a new card generated randomly
+new_card()
 
 window.mainloop()
